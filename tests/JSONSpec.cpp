875f2f77ad5d0a6888409283df8a2c29677d0f83
Jeremy List <quick.dudley@gmail.com>
2023-09-01 15:19:57 +1200

    run formatter

--- tests/JSONSpec.cpp
+++ tests/JSONSpec.cpp
@@ -1,7 +1,7 @@
 #include "JSON.h"
 #include <catch2/catch_all.hpp>
 
-TEST_CASE( "Handles unicode", "[JSON::escapeString]") {
+TEST_CASE("Handles unicode", "[JSON::escapeString]") {
   BString source("🀐");
   BString result = JSON::escapeString(source);
   REQUIRE(result == "\"🀐\"");
