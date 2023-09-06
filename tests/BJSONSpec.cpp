7d5f96c661caea1f42fdfda3f37d323638d591f0
Jeremy List <quick.dudley@gmail.com>
2023-09-07 09:38:27 +1200

    Autoformat

--- tests/BJSONSpec.cpp
+++ tests/BJSONSpec.cpp
@@ -4,23 +4,23 @@
 TEST_CASE("Can represent two objects in arrays", "[BJSON]") {
   BMessage target;
   {
-  	JSON::RootSink sink(new JSON::BMessageDocSink(&target));
-  	BString k, v;
-  	sink.beginArray(k);
-  	sink.beginObject(k);
-  	k = "a";
-  	v = "b";
-  	sink.addString(k, v);
-  	k = "c";
-  	v = "d";
-  	sink.addString(k, v);
-  	sink.closeNode();
-  	sink.beginObject(k);
-  	k = "e";
-  	v = "f";
-  	sink.addString(k, v);
-  	sink.closeNode();
-  	sink.closeNode();
+    JSON::RootSink sink(new JSON::BMessageDocSink(&target));
+    BString k, v;
+    sink.beginArray(k);
+    sink.beginObject(k);
+    k = "a";
+    v = "b";
+    sink.addString(k, v);
+    k = "c";
+    v = "d";
+    sink.addString(k, v);
+    sink.closeNode();
+    sink.beginObject(k);
+    k = "e";
+    v = "f";
+    sink.addString(k, v);
+    sink.closeNode();
+    sink.closeNode();
   }
   REQUIRE(target.what == 'JSAR');
   BMessage inner;
--- tests/ValidationDatasetSpec.cpp
+++ tests/ValidationDatasetSpec.cpp
@@ -11,17 +11,16 @@ TEST_CASE("Validation matches examples",
   REQUIRE(sodium_init() >= 0);
   BMessage examples;
   {
-  	JSON::Parser parser(new JSON::BMessageDocSink(&examples));
-  	BFile input("tests/data.json", B_READ_ONLY);
-  	char buffer[1024];
-  	ssize_t readBytes;
-  	while(input.Read(buffer, sizeof(buffer)) > 0) {
-  	  for(int i = 0; i < readBytes; i++) {
-  	  	parser.nextChar(buffer[i]);
-  	  }
-  	}
+    JSON::Parser parser(new JSON::BMessageDocSink(&examples));
+    BFile input("tests/data.json", B_READ_ONLY);
+    char buffer[1024];
+    ssize_t readBytes;
+    while (input.Read(buffer, sizeof(buffer)) > 0) {
+      for (int i = 0; i < readBytes; i++) {
+        parser.nextChar(buffer[i]);
+      }
+    }
   }
   examples.PrintToStream();
   REQUIRE(false);
 }
-
