3db54a99308a2f22818e865d85251f78d2a66a67
Jeremy List <quick.dudley@gmail.com>
2023-09-13 10:22:43 +1200

    Make test suite run a BApplication in the background.

--- tests/TestSetup.cpp
+++ tests/TestSetup.cpp
@@ -1,7 +1,18 @@
+#include <Application.h>
 #include <catch2/reporters/catch_reporter_event_listener.hpp>
 #include <catch2/reporters/catch_reporter_registrars.hpp>
 #include <sodium.h>
 #include <stdexcept>
+
+static BApplication *testApp = NULL;
+static thread_id testAppThread;
+
+static int32 runApp(void *ignored) {
+  testApp = new BApplication("application/x-vnd.habitat-test-suite");
+  testApp->Run();
+  delete testApp;
+  return 0;
+}
 
 class TestSetup : public Catch::EventListenerBase {
 public:
@@ -10,6 +21,19 @@ public:
   void testRunStarting(Catch::TestRunInfo const &) override {
     if (sodium_init() < 0)
       throw std::runtime_error("Failed to initialize libsodium");
+    testAppThread = spawn_thread(runApp, "Test BApplication thread",
+                                 B_NORMAL_PRIORITY, NULL);
+    resume_thread(testAppThread);
+  }
+
+  void testRunEnded(Catch::TestRunStats const &) override {
+    if (testApp != NULL) {
+      testApp->Lock();
+      testApp->Quit();
+      testApp->Unlock();
+      status_t exitValue;
+      wait_for_thread(testAppThread, &exitValue);
+    }
   }
 };
 
