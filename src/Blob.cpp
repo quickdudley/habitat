e8615654a5aa3e8b13c18e0de6345f708ae6ebd8
Jeremy List <quick.dudley@gmail.com>
2023-10-16 13:55:54 +1300

    WIP on blob sync (initial message routing)

--- src/Blob.cpp
+++ src/Blob.cpp
@@ -1,9 +1,38 @@
 #include "Blob.h"
+#include <NodeMonitor.h>
 #include <algorithm>
 
 namespace blob {
 
-void Wanted::MessageReceived(BMessage *message) {}
+void Wanted::MessageReceived(BMessage *message) {
+  switch(message->what) {
+  	case B_QUERY_UPDATE: {
+  	  if (message->GetInt32("opcode", B_ERROR) == B_ENTRY_CREATED) {
+  	  	entry_ref ref;
+  	  	ref.device = message->GetInt32("device", B_ERROR);
+  	  	ref.directory = message->GetInt64("directory", B_ERROR);
+  	  	BString name;
+  	  	if (message->FindString("name", &name) == B_OK)
+  	      ref.set_name(name.String());
+  	    BEntry entry(&ref);
+  	    if (entry.InitCheck() == B_OK && entry.Exists()) {
+  	      BNode node(&ref);
+  	      BString cypherkey;
+  	      if (node.ReadAttrString("cypherkey", &cypherkey) != B_OK)
+  	        return;
+  	      for (auto item = this->wanted.begin(); item != this->wanted.end(); item++) {
+  	      	if (std::get<0>(*item) == cypherkey) {
+  	      	  for (auto &target : std::get<3>(*item))
+  	      	    target.SendMessage(message);
+  	      	  this->wanted.erase(item);
+  	      	  return;
+  	      	}
+  	      }
+  	    }
+  	  }
+  	} break;
+  }
+}
 
 void Wanted::addWant(BString &cypherkey, int8 distance, BMessenger replyTo) {
   for (auto &item : this->wanted) {
