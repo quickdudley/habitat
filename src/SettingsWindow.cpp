a67576ad07ed754cfe74f80445f71ce3c0517b29
Jeremy List <quick.dudley@gmail.com>
2023-11-08 15:39:52 +1300

    Put temporary data in the server list.

--- src/SettingsWindow.cpp
+++ src/SettingsWindow.cpp
@@ -1,6 +1,7 @@
 #include "SettingsWindow.h"
 #include <Button.h>
 #include <Catalog.h>
+#include <ControlLook.h>
 #include <GroupLayout.h>
 #include <ListView.h>
 #include <LocaleRoster.h>
@@ -19,11 +20,29 @@ namespace {
 class NetworkTab : public BView {
 public:
   NetworkTab(BRect contentFrame);
+  void AttachedToWindow();
+  void MessageReceived(BMessage *msg) override;
 
 private:
   BListView *serverList;
-  BButton *addButton;
-  BButton *delButton;
+  BTextControl *addrControl;
+  BTextControl *keyControl;
+  BButton *saveButton;
+};
+
+class ServerEntry : public BListItem {
+public:
+  ServerEntry(const BString &netAddress = "", const BString cypherkey = "");
+  void setAddress(const BString &netAddress);
+  void setCypherkey(const BString &cypherkey);
+  void DrawItem(BView *owner, BRect frame, bool complete = false) override;
+  void Update(BView *owner, const BFont *font) override;
+  const BString &getAddress();
+  const BString &getCypherkey();
+
+private:
+  BString netAddress;
+  BString cypherkey;
 };
 
 NetworkTab::NetworkTab(BRect contentFrame)
@@ -31,6 +50,9 @@ NetworkTab::NetworkTab(BRect contentFrame)
     BView(contentFrame, B_TRANSLATE("Network"), B_FOLLOW_ALL_SIDES,
           B_WILL_DRAW) {
   this->AdoptSystemColors();
+}
+
+void NetworkTab::AttachedToWindow() {
   auto outerLayout = new BGroupLayout(B_HORIZONTAL);
   this->SetLayout(outerLayout);
   {
@@ -38,26 +60,89 @@ NetworkTab::NetworkTab(BRect contentFrame)
     outerLayout->AddItem(column1Layout);
     this->serverList = new BListView("ServerList");
     column1Layout->AddView(this->serverList);
+    this->serverList->AddItem(new ServerEntry("testAddress", "testKey"), 0);
     {
       auto buttonLayout = new BGroupLayout(B_HORIZONTAL);
       column1Layout->AddItem(buttonLayout, 0.2);
-      this->addButton = new BButton(B_TRANSLATE("Add"), new BMessage('ASRV'));
-      buttonLayout->AddView(this->addButton);
-      this->delButton =
-          new BButton(B_TRANSLATE("Remove"), new BMessage('DSRV'));
-      buttonLayout->AddView(this->delButton);
+      auto addButton = new BButton(B_TRANSLATE("Add"), new BMessage('ASRV'));
+      addButton->SetTarget(this);
+      buttonLayout->AddView(addButton);
+      auto delButton = new BButton(B_TRANSLATE("Remove"), new BMessage('DSRV'));
+      delButton->SetTarget(this);
+      buttonLayout->AddView(delButton);
     }
   }
   outerLayout->AddView(new BSeparatorView(B_VERTICAL));
   {
     auto column2Layout = new BGroupLayout(B_VERTICAL);
     outerLayout->AddItem(column2Layout);
-    column2Layout->AddView(
-        new BTextControl(B_TRANSLATE("Network address"), "", NULL));
-    column2Layout->AddView(
-        new BTextControl(B_TRANSLATE("Public key"), "", NULL));
+    this->addrControl = new BTextControl(B_TRANSLATE("Network address"), "",
+                                         new BMessage('NADR'));
+    this->addrControl->SetTarget(this);
+    this->addrControl->SetEnabled(false);
+    column2Layout->AddView(this->addrControl);
+    this->keyControl =
+        new BTextControl(B_TRANSLATE("Public key"), "", new BMessage('CKEY'));
+    this->keyControl->SetEnabled(false);
+    this->keyControl->SetTarget(this);
+    column2Layout->AddView(this->keyControl);
+    {
+      auto buttonLayout = new BGroupLayout(B_HORIZONTAL);
+      column2Layout->AddItem(buttonLayout);
+      buttonLayout->AddItem(BSpaceLayoutItem::CreateVerticalStrut(1));
+      this->saveButton = new BButton(B_TRANSLATE("Save"), new BMessage('SSRV'));
+      this->saveButton->SetTarget(this);
+      this->saveButton->SetEnabled(false);
+      buttonLayout->AddView(saveButton);
+    }
     column2Layout->AddItem(BSpaceLayoutItem::CreateGlue());
   }
+}
+
+void NetworkTab::MessageReceived(BMessage *message) {
+  BView::MessageReceived(message);
+}
+
+ServerEntry::ServerEntry(const BString &netAddress, const BString cypherkey)
+    :
+    netAddress(netAddress),
+    cypherkey(cypherkey) {}
+
+void ServerEntry::setAddress(const BString &netAddress) {
+  this->netAddress = netAddress;
+}
+
+void ServerEntry::setCypherkey(const BString &cypherkey) {
+  this->cypherkey = cypherkey;
+}
+
+const BString &ServerEntry::getAddress() { return this->netAddress; }
+
+const BString &ServerEntry::getCypherkey() { return this->cypherkey; }
+
+void ServerEntry::DrawItem(BView *owner, BRect frame, bool complete) {
+  rgb_color lowColor = owner->LowColor();
+  rgb_color color = IsSelected() ? ui_color(B_LIST_SELECTED_BACKGROUND_COLOR)
+                                 : owner->ViewColor();
+  owner->SetLowColor(color);
+  owner->FillRect(frame, B_SOLID_LOW);
+  font_height fontHeight;
+  owner->GetFontHeight(&fontHeight);
+  owner->MovePenTo(frame.left + be_control_look->DefaultLabelSpacing(),
+                   frame.top + fontHeight.leading + fontHeight.ascent);
+  owner->DrawString(this->netAddress);
+  owner->MovePenTo(frame.left + be_control_look->DefaultLabelSpacing(),
+                   frame.top + (fontHeight.leading + fontHeight.ascent) * 2 +
+                       fontHeight.descent);
+  owner->DrawString(this->cypherkey);
+  owner->SetLowColor(lowColor);
+}
+
+void ServerEntry::Update(BView *owner, const BFont *font) {
+  font_height fontHeight;
+  font->GetHeight(&fontHeight);
+  this->SetHeight(
+      (fontHeight.ascent + fontHeight.descent + fontHeight.leading) * 2.1);
 }
 }; // namespace
 
