cd6ea84db35be5416e1ad42b38f202599f263fae
Jeremy List <quick.dudley@gmail.com>
2024-01-08 11:39:50 +1300

    Untangle after mistakes with git

--- src/Updatable.h
+++ src/Updatable.h
@@ -1,39 +1,7 @@
-<<<<<<< Updated upstream
 #ifndef UPDATABLE_H
 #define UPDATABLE_H
 
-template <class T> class Updatable {
-public:
-  Updatable(const T &value)
-      :
-      value(value) {}
-  T &peek() { return value; }
-  void put(const T &value, int64 weight) {
-    if (weight > this->weight) {
-      this->value = value;
-      this->weight = weight;
-    }
-  }
-  template <class F> bool check(F &&producer, int64 weight) {
-    if (weight > this->weight) {
-      if (producer(this->value)) {
-        this->weight = weight;
-        return true;
-      }
-    }
-    return false;
-  }
-
-private:
-  T value;
-  int64 weight = -1;
-};
-
-#endif // UPDATABLE_H
-||||||| Stash base
-=======
-#ifndef UPDATABLE_H
-#define UPDATABLE_H
+#include <SupportDefs.h>
 
 template <class T> class Updatable {
 public:
@@ -64,4 +32,3 @@ private:
 
 #endif // UPDATABLE_H
 
->>>>>>> Stashed changes
