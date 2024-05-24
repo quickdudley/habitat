74d99ac10fd8cd4d48c27e2d154829c8d4f01f0d
Jeremy List <jeremylist@thunix.net>
2024-05-24 16:21:03 +1200

    Add failing test (JSON number parsing)

--- src/Post.cpp
+++ src/Post.cpp
@@ -12,7 +12,6 @@
 #include <StringList.h>
 #include <cstring>
 #include <ctime>
-#include <iostream>
 #include <unicode/utf8.h>
 #include <variant>
 #include <vector>
@@ -1315,7 +1314,6 @@ static inline status_t validatePrevious(BMessage *message, BString &lastID) {
       return B_BAD_VALUE;
     return B_OK;
   }
-  return B_BAD_VALUE;
 }
 
 static inline status_t validateHash(BMessage *message) {
