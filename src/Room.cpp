6ae4c3cb1a99de385e4eb862f2ae4255cc301276
Jeremy List <jeremylist@thunix.net>
2025-04-01 12:02:33 +1300

    Remove initiating room peer from connected list when it disconnects

--- src/Room.cpp
+++ src/Room.cpp
@@ -273,9 +273,15 @@ MkTunnel::MkTunnel(ConnectedList *connectedList)
 status_t MkTunnel::call(muxrpc::Connection *connection,
                         muxrpc::RequestType type, BMessage *args,
                         BMessenger replyTo, BMessenger *inbound) {
+  std::function<void()> cleanupHook;
   if (BMessage arg0; args->FindMessage("0", &arg0) == B_OK) {
-    if (BString origin; arg0.FindString("origin", &origin) == B_OK)
-      this->connectedList->addConnected(origin);
+    if (BString origin; arg0.FindString("origin", &origin) == B_OK) {
+      auto connectedList = this->connectedList;
+      connectedList->addConnected(origin);
+      cleanupHook = [connectedList, origin]() {
+        connectedList->rmConnected(origin);
+      };
+    }
   }
   auto tunnel = new Tunnel(replyTo);
   auto reader = new TunnelReader(tunnel);
@@ -283,7 +289,7 @@ status_t MkTunnel::call(muxrpc::Connection *connection,
   connection->AddHandler(reader);
   connection->Unlock();
   *inbound = BMessenger(reader);
-  dynamic_cast<Habitat *>(be_app)->acceptConnection(tunnel);
+  dynamic_cast<Habitat *>(be_app)->acceptConnection(tunnel, cleanupHook);
   return B_OK;
 }
 } // namespace
